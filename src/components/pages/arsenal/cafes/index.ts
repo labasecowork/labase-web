55c72ac8997c2e945df9b31763c2f4644af06b43
michxlmg <jhefrigasparalmonacid0@gmail.com>
2025-08-26 01:52:05 -0500

    feat: ✨ add desserts, breakfast, coffee, and juice sections; update entrepreneurship pages and content

---
+++ src/components/pages/arsenal/cafes/index.ts
@@ -0,0 +1,3 @@
+export { default as CoffiSection } from "./coffi-section/index.astro";
+export { default as MenuCoffi } from "./menu-section/index.astro";
+export { default as HeroCoffi } from "./hero-section/index.astro";
---
+++ src/components/pages/arsenal/home/index.ts
@@ -0,0 +1,2 @@
+export { default as GallerySection } from "./gallery-section/index.astro";
+export { default as MenuSection } from "./menu-section/index.astro";
